4c64aff53918cc27a8000f1bf998ca91a3599a0a
sfr23 <46600928+sfr23@users.noreply.github.com>
2019-02-04 16:37:59 +0100

    create

---
+++ install-iis.ps1
@@ -0,0 +1,8 @@
+# Install IIS.
+dism /online /enable-feature /featurename:IIS-WebServerRole
+
+# Set the home page.
+Set-Content `
+  -Path "C:\\inetpub\\wwwroot\\Default.htm" `
+  -Value "<html><body><h2>Welcome to Azure LAB! My name is $($env:computername).</h2></body></html>"
+  
